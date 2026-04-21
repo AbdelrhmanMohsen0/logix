2ab95fc8ac7fa3c46741dcd738f6129a356eb33e
a7med-3li <ahmedmohamedalyyy@gmail.com>
2026-04-22 01:27:48 +0200

    refactor: remove unused imports in OrderController

--- order-service/src/main/java/com/core/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/core/orderservice/controller/OrderController.java
@@ -9,10 +9,6 @@ import com.core.orderservice.dto.OrderSummaryDTO;
 import com.core.orderservice.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
