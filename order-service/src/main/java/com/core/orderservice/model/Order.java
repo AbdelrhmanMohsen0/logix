f44923e172c86d84780ecbc693cecd9ad93dce47
a7med-3li <ahmedmohamedalyyy@gmail.com>
2026-04-21 17:15:01 +0200

    - updated the order table name to avoid interception with keywords - add JPA config to fix dialect error

--- order-service/src/main/java/com/core/orderservice/model/Order.java
+++ order-service/src/main/java/com/core/orderservice/model/Order.java
@@ -13,15 +13,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.Valid;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "orders")
 public class Order {
 	
 	@Id
