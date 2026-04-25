0af4c4fad15c0739f26b1d48e5e4fccf144b5aea
Abdelrhman Mohsen <abdulrahman_20230317@fci.helwan.edu.eg>
2026-04-25 20:45:40 +0300

    Configure spring security in order-service

--- order-service/src/main/java/com/core/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/core/orderservice/controller/OrderController.java
@@ -2,19 +2,18 @@ package com.core.orderservice.controller;
 
 import java.util.List;
 import java.util.UUID;
-import com.core.orderservice.domain.OrderStatus;
 import com.core.orderservice.dto.OrderDTO;
 import com.core.orderservice.dto.OrderRequest;
 import com.core.orderservice.dto.OrderSummaryDTO;
 import com.core.orderservice.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,20 +24,25 @@ public class OrderController {
 	
 	private final OrderService orderService;
 	
-	@PostMapping("/order")
-	public OrderDTO createOrder(@Valid @RequestBody OrderRequest order, @RequestHeader("X-Organization-ID") UUID organizationId) {
-		return orderService.createOrder(order, organizationId);
+	@PostMapping
+	@PreAuthorize("hasRole('SALES')")
+	public OrderDTO createOrder(@Valid @RequestBody OrderRequest order, JwtAuthenticationToken auth) {
+		UUID orgId = UUID.fromString(auth.getTokenAttributes().get("org").toString());
+		return orderService.createOrder(order, orgId);
 	}
 	
-	@GetMapping("/")
-	public List<OrderSummaryDTO> getOrdersSummary(@RequestHeader("X-Organization-ID") UUID organizationId){
-		return orderService.getOrdersSummary(organizationId);
+	@GetMapping
+	@PreAuthorize("hasRole('SALES')")
+	public List<OrderSummaryDTO> getOrderSummaries(JwtAuthenticationToken auth){
+		UUID orgId = UUID.fromString(auth.getTokenAttributes().get("org").toString());
+		return orderService.getOrderSummaries(orgId);
 	}
 	
 	@GetMapping("/{id}")
-	public OrderDTO getOrder(@RequestHeader("X-Organization-ID") UUID organizationId,
-			@PathVariable UUID id) {
-		return orderService.getOrder(organizationId, id);
+	@PreAuthorize("hasRole('SALES')")
+	public OrderDTO getOrder(JwtAuthenticationToken auth, @PathVariable UUID id) {
+		UUID orgId = UUID.fromString(auth.getTokenAttributes().get("org").toString());
+		return orderService.getOrder(orgId, id);
 	}
-	
+
 }
